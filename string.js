05d1e3f896cdcda283e4daa1c09811fc57dc133c
Vikas Verma <vikasverma2102@gmail.com>
2025-10-28 11:59:46 +0530

    Find Words Containing Character

--- string.js
+++ string.js
@@ -22,7 +22,30 @@ const countLastWordLength = (s) => {
   return c;
 };
 
-console.log(countLastWordLength(str));
-console.log(countLastWordLength(str1));
-console.log(countLastWordLength(str2));
-console.log(countLastWordLength(str3));
+// console.log(countLastWordLength(str));
+// console.log(countLastWordLength(str1));
+// console.log(countLastWordLength(str2));
+// console.log(countLastWordLength(str3));
+
+let words = ["leet", "code"]; //x = "e"
+let words1 = ["abc", "bcd", "aaaa", "cbc"]; //x = "a"
+let words2 = ["abc", "bcd", "aaaa", "cbc"]; //x = "z"
+
+const findWordsContaining = (w, x) => {
+  let output = [];
+  for (let i = 0; i < w.length; i++) {
+    // console.log(w[i]);
+    for (let j = 0; j < w[i].length; j++) {
+      if (w[i][j] === x) {
+        // console.log(w[i][j], x);
+        output.push(i);
+        break;
+      }
+    }
+  }
+  return output;
+};
+
+console.log(findWordsContaining(words, "e"));
+console.log(findWordsContaining(words1, "a"));
+console.log(findWordsContaining(words2, "z"));
